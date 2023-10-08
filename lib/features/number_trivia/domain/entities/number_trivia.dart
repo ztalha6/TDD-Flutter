14401bfd86d8bfb83793cc1f5568793f1d2e4555
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-10-08 22:23:30 +0500

    entity added

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class NumberTrivia extends Equatable {
+  const NumberTrivia({
+    required this.text,
+    required this.number,
+  });
+
+  final String? text;
+  final int? number;
+
+  @override
+  String toString() {
+    return "$text, $number, ";
+  }
+
+  @override
+  List<Object?> get props => [
+        text,
+        number,
+      ];
+}
